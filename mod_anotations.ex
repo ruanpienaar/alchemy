b4cd908546e241bf9ce050e2d47b41b4d79c49f5
ruanpienaar <ruan800@gmail.com>
2015-08-14 15:39:07 +0100

    ..

---
+++ mod_anotations.ex
@@ -0,0 +1,13 @@
+defmodule Mod_Anotations do
+
+	@vsn 2
+	@moduledoc """
+	Provides some module documentation...
+	"""
+	@doc """
+	documentation for some_func...
+	"""
+	def some_func() do
+	end
+
+end
